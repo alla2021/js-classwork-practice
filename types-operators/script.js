156f5cb015abc5a241cd6f26546ba2bf7cf5f709
Alexey Usanin <aleksej.usanin@icloud.com>
2021-09-09 21:22:03 +0300

    Types and Operators

---
+++ types-operators/script.js
@@ -0,0 +1,89 @@
+// alert('Hello');
+
+// const question = prompt('Какой день сегодня?');
+
+// console.log(question);
+
+// const quest = confirm('Are you sure?')
+// console.log(quest)
+
+// const age = '23';
+// const firstName = "Harry";
+// const lastName = "Potter";
+
+// const fullName = `${firstName} ${lastName} is ${age} year old`;
+
+// const sum = 4 + 3;
+
+
+///// String
+const str = 'Hello';
+
+console.log(str.slice(0, -2))
+
+
+/// Number
+const num = '34ddfdfgdfg';
+
+console.log(Number(num))
+
+/// BigIng
+const max = 999999999999999999999999999999999999999999999999n;
+
+console.log(max);
+
+/// NUll
+let value = null;
+
+/// Undefined
+const value1 = undefined;
+
+console.log(value1)
+
+/// Boolean
+const boolFalse = false;
+const boolTrue = true;
+
+// Object
+const obj = { name: 'mike', age: 33, sym: 'dfdfdf' };
+
+/// Symbol
+const sym = Symbol('piter');
+
+
+/// typeof
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof max);
+console.log(typeof value);
+console.log(typeof value1);
+console.log(typeof boolFalse);
+console.log(typeof obj);
+console.log(typeof sym);
+
+// Conditional
+
+if (boolFalse) {
+  console.log('It is true')
+} else if (boolFalse) {
+  console.log('true')
+} else if (true) {
+  console.log('yeaah')
+} else if (true) {
+} else {
+
+}
+
+// Logical operators
+// &&  И
+// || Или
+// != Не
+
+
+if (boolFalse || boolTrue) {
+
+}
+
+if (!boolFalse) {
+
+}
