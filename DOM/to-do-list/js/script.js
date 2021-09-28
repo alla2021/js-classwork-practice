91b95d81780c22d02c31707105bac1f8a79881bb
Alexey Usanin <aleksej.usanin@icloud.com>
2021-09-28 21:49:58 +0300

    Events

--- DOM/to-do-list/js/script.js
+++ DOM/to-do-list/js/script.js
@@ -16,13 +16,15 @@ function renderTaskElement(item) {
     <button class="list__item-btn is-complete">Complete</button>
     <button class="list__item-btn is-delete">Delete</button>
   </div>`;
+  const inputField = document.createElement('input');
 
   listItem.classList.add('list__item');
   itemText.classList.add('list__item-text');
   itemText.innerText = item.title;
 
-
+  inputField.style.display = 'none';
   listItem.append(itemText);
+  listItem.append(inputField);
   listItem.insertAdjacentHTML('beforeend', actions);
 
   listItem.querySelector('.is-complete').addEventListener('click', function () {
@@ -33,15 +35,26 @@ function renderTaskElement(item) {
     this.closest('.list__item').remove()
   })
 
+  itemText.addEventListener('click', function() {
+    this.style.display = 'none';
+    inputField.style.display = 'block';
+    inputField.value = this.innerText;
+  })
+
+  inputField.addEventListener('change', function () {
+    this.style.display = 'none';
+    itemText.innerText = this.value;
+    itemText.style.display = 'block';
+  })
+
   return listItem;
 }
-
 
 function addNewTask(task) {
   const list = document.querySelector('#to-do-list');
 
-  list.append(task)
-}
+  list.append(task);
+};
 
 // function renderListTask(arr) {
 //   for (let item of arr) {
@@ -75,7 +88,7 @@ inputTask.addEventListener('input', function (event) {
   } else {
     addTaskBtn.setAttribute('disabled', true)
   }
-})
+});
 
 addTaskBtn.addEventListener('click', function () {
   const value = this.previousElementSibling.value;
@@ -83,5 +96,150 @@ addTaskBtn.addEventListener('click', function () {
   createNewTask(value);
   inputTask.value = '';
   addTaskBtn.setAttribute('disabled', true)
+});
+
+
+
+const link = document.querySelector('#link');
+const form = document.querySelector('#form');
+
+link.addEventListener('click', function (event) {
+  console.log(`event`, event)
+  event.preventDefault()
 })
 
+
+form.addEventListener('submit', function(event) {
+  console.log(`event`, event)
+  event.preventDefault();
+})
+
+function keyPress() {
+  document.addEventListener('keydown', function (event) {
+    console.log(`event`, event)
+    event.preventDefault();
+
+      // console.log(`event.code`, event.code)
+      if (event.ctrlKey && event.code === 'KeyR') {
+        console.log(`ControlLeft and KeyR`)
+      }
+  })
+}
+
+keyPress()
+
+// document.removeEventListener('keydown', keyPress)
+
+const box = document.querySelector('#box');
+
+box.addEventListener('mouseenter', function(event) {
+  console.log(event)
+
+  const offsetX = event.offsetX;
+
+
+  this.addEventListener('mousemove', function() {
+    console.log('Mouse enter and move')
+  })
+
+})
+
+
+
+// const playList = [
+//   {
+
+//    author: "LED ZEPPELIN",
+//    song:"STAIRWAY TO HEAVEN"
+
+//   },
+
+//   {
+
+//    author: "QUEEN",
+//    song:"BOHEMIAN RHAPSODY"
+
+//   },
+//   {
+
+//    author: "LYNYRD SKYNYRD",
+//    song:"FREE BIRD"
+
+//   },
+
+//   {
+
+//    author: "DEEP PURPLE",
+//    song:"SMOKE ON THE WATER"
+
+//   },
+//   {
+
+//    author: "JIMI HENDRIX",
+//    song:"ALL ALONG THE WATCHTOWER"
+
+//   },
+//   {
+
+//    author: "AC/DC",
+//    song:"BACK IN BLACK"
+
+//   },
+
+//   {
+
+//    author: "QUEEN",
+
+//    song:"WE WILL ROCK YOU"
+
+//   },
+
+//   {
+
+//    author: "METALLICA",
+
+//    song:"ENTER SANDMAN"
+
+//   }
+// ];
+
+
+// function printSongList(arr) {
+//   const list = document.querySelector('#songs');
+
+//   for (let item of arr) {
+//     const listItem = document.createElement('li');
+//     listItem.innerText = item.song;
+
+//     list.append(listItem);
+//   }
+// }
+
+// printSongList(playList);
+
+// const modalBtn = document.getElementById('modal-btn');
+// const modal = document.getElementById('modal');
+
+
+// modalBtn.addEventListener('click', function () {
+//   modal.classList.add('is--open');
+// })
+
+
+// const red = document.querySelector('.red');
+// const switchBtn = document.querySelector('#switch');
+// // const lights = document.querySelectorAll('.light')
+
+// switchBtn.addEventListener('click', function () {
+//   const isRedActive = red.classList.contains('is--active');
+//   const isGreenActive = red.classList.contains('is--active');
+
+//   if (!isRedActive) {
+//     red.classList.add('is--active');
+//   }
+
+//   if (!isGreenActive) {
+//     red.classList.remove('is--active');
+//     green.classList.add('is--active');
+//   }
+// })
